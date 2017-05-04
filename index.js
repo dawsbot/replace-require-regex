11a7fea29295a9689c4b94cac24972f0b4297388
Dawson Botsford <dawsonb@uber.com>
2017-05-04 10:52:26 -0700

    🎨 Simplify regex

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 'use strict';
 module.exports = (content, from, to) => {
+  // require and import replacement regex patterns
   const regexes = [
-    new RegExp(`(require\\(['|"])(${from})(.*?['|"]\\))`, 'g'),
-    new RegExp(`(from\\s+['|"])(${from})(.*?['|"])`, 'g')
+    new RegExp(`(require\\(['|"])${from}(.*?['|"]\\))`, 'g'),
+    new RegExp(`(from\\s+['|"])${from}(.*?['|"])`, 'g')
   ];
   regexes.forEach(reg => {
     const res = reg.exec(content);
     if (res) {
       const before = res[1];
-      const after = res[3];
+      const after = res[2];
       const wrappedTo = `${before}${to}${after}`;
       content = content.replace(res[0], wrappedTo);
     }
