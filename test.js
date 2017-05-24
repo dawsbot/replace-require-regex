bcf346103c893b018bb83e1f0ca71581338d9c32
Dawson Botsford <3408480+dawsbot@users.noreply.github.com>
2017-05-24 16:53:47 -0700

    ✨ Multi replace (#2)
    * handle multiple matches
    
    * lint fix

--- test.js
+++ test.js
@@ -10,6 +10,16 @@ const requireTestCases = [
     out: `const a = require('${to}');`
   },
   {
+    in: `
+    const a = require('${from}');
+    const b = require('${from}/b');
+    `,
+    out: `
+    const a = require('${to}');
+    const b = require('${to}/b');
+    `
+  },
+  {
     in: `const a = require("${from}");`,
     out: `const a = require("${to}");`
   },
@@ -40,12 +50,18 @@ const importTestCases = [
     out: `import a from '${to}'`
   },
   {
-    in: `import a from "${from}"`,
-    out: `import a from "${to}"`
+    in: `
+    import a from "${from}";
+    import b from "${from}/b";
+    `,
+    out: `
+    import a from "${to}";
+    import b from "${to}/b";
+    `
   },
   {
-    in: `import a from '${from}/extra'`,
-    out: `import a from '${to}/extra'`
+    in: `import a from '${from}/extra';`,
+    out: `import a from '${to}/extra';`
   },
   {
     in: `import a from './${from}'`,
