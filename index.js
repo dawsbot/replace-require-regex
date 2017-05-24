8409bb1be98507286808c4b4cc549eba7fa4533f
Dawson Botsford <dawsonbotsford@gmail.com>
2017-05-24 16:53:47 -0700

    ✨ Multi replace (#2)
    * handle multiple matches
    
    * lint fix

--- index.js
+++ index.js
@@ -8,12 +8,13 @@ module.exports = (content, from, to) => {
     new RegExp(`(from\\s+['|"])${from}(.*?['|"])`, 'g')
   ];
   regexes.forEach(reg => {
-    const res = reg.exec(content);
-    if (res) {
-      const before = res[1];
-      const after = res[2];
+    let match;
+    // eslint-disable-next-line no-cond-assign
+    while (match = reg.exec(content)) {
+      const before = match[1];
+      const after = match[2];
       const wrappedTo = `${before}${to}${after}`;
-      content = content.replace(res[0], wrappedTo);
+      content = content.replace(match[0], wrappedTo);
     }
   });
   return content;
